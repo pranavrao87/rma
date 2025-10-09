a80816aba563b3e1d2b63af323498e7798414b85
Pranav Rao <pranav.rao@utexas.edu>
2025-10-09 17:51:10 -0500

    added priv_obs to obs_group

--- source/rma_tasks/rma_tasks/rma/config/spot/agents/ppo_agent_cfg.py
+++ source/rma_tasks/rma_tasks/rma/config/spot/agents/ppo_agent_cfg.py
@@ -18,6 +18,7 @@ class Rma1PPORunnerCfg(RslRlOnPolicyRunnerCfg):
     store_code_state = False
     logger = "wandb"
     wandb_project = "Vision_RMA"
+    obs_group = {"policy" : ["policy"], "priv_obs" : ["priv_obs"]}
     policy = BasePolicyCfg(
         class_name="BasePolicy",
         prev_step_size=48,
