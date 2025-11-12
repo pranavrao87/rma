2d008fec08f91e0c7c301975aaf6af2c4f656be4
pranavrao87 <pranav.rao.aus@gmail.com>
2025-11-11 19:14:52 -0600

    small changes

--- source/rma_tasks/rma_tasks/rma/runners/adaptation_module_runner.py
+++ source/rma_tasks/rma_tasks/rma/runners/adaptation_module_runner.py
@@ -159,27 +159,27 @@ class DistillationRunner(OnPolicyRunner):
         iteration_time = locs["collection_time"] + locs["learn_time"]
 
         # -- Episode info
-        ep_string = ""
-        if locs["ep_infos"]:
-            for key in locs["ep_infos"][0]:
-                infotensor = torch.tensor([], device=self.device)
-                for ep_info in locs["ep_infos"]:
-                    # handle scalar and zero dimensional tensor infos
-                    if key not in ep_info:
-                        continue
-                    if not isinstance(ep_info[key], torch.Tensor):
-                        ep_info[key] = torch.Tensor([ep_info[key]])
-                    if len(ep_info[key].shape) == 0:
-                        ep_info[key] = ep_info[key].unsqueeze(0)
-                    infotensor = torch.cat((infotensor, ep_info[key].to(self.device)))
-                value = torch.mean(infotensor)
-                # log to logger and terminal
-                if "/" in key:
-                    self.writer.add_scalar(key, value, locs["it"])
-                    ep_string += f"""{f'{key}:':>{pad}} {value:.4f}\n"""
-                else:
-                    self.writer.add_scalar("Episode/" + key, value, locs["it"])
-                    ep_string += f"""{f'Mean episode {key}:':>{pad}} {value:.4f}\n"""
+        # ep_string = ""
+        # if locs["ep_infos"]:
+        #     for key in locs["ep_infos"][0]:
+        #         infotensor = torch.tensor([], device=self.device)
+        #         for ep_info in locs["ep_infos"]:
+        #             # handle scalar and zero dimensional tensor infos
+        #             if key not in ep_info:
+        #                 continue
+        #             if not isinstance(ep_info[key], torch.Tensor):
+        #                 ep_info[key] = torch.Tensor([ep_info[key]])
+        #             if len(ep_info[key].shape) == 0:
+        #                 ep_info[key] = ep_info[key].unsqueeze(0)
+        #             infotensor = torch.cat((infotensor, ep_info[key].to(self.device)))
+        #         value = torch.mean(infotensor)
+        #         # log to logger and terminal
+        #         if "/" in key:
+        #             self.writer.add_scalar(key, value, locs["it"])
+        #             ep_string += f"""{f'{key}:':>{pad}} {value:.4f}\n"""
+        #         else:
+        #             self.writer.add_scalar("Episode/" + key, value, locs["it"])
+        #             ep_string += f"""{f'Mean episode {key}:':>{pad}} {value:.4f}\n"""
 
         mean_std = self.alg.policy.action_std.mean()
         fps = int(collection_size / (locs["collection_time"] + locs["learn_time"]))
@@ -225,7 +225,7 @@ class DistillationRunner(OnPolicyRunner):
             for key, value in locs["loss_dict"].items():
                 log_string += f"""{f'{key}:':>{pad}} {value:.4f}\n"""
 
-        log_string += ep_string
+        # log_string += ep_string
         log_string += (
             f"""{'-' * width}\n"""
             f"""{'Total timesteps:':>{pad}} {self.tot_timesteps}\n"""
